f3c524097fdfaf4c5f60f487dfa0c002804ff4d1
JINODK <jinodk2003@gmail.com>
2021-02-18 16:02:57 +0700

    Optimize code and add comment
    Optimize the code a little bit to make the display UI more readable and add a bunch of comment for explanation

--- Jtag_RX/Jtag_RX.ino
+++ Jtag_RX/Jtag_RX.ino
@@ -19,21 +19,31 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
+#include <ESP8266WiFi.h>
+void preinit() {
+  // Global WiFi constructors are not called yet
+  // (global class instances like WiFi, Serial... are not yet initialized)..
+  // No global object methods or C++ exceptions can be called in here!
+  // The below is a static class method, which is similar to a function, so it's ok.
+  ESP8266WiFiClass::preinitWiFiOff(); // turn off wifi AP as we don't need that
+}
 #include <LoRa.h>
 #include <SPI.h>
-#include <AESLib.h>
+#include <AESLib.h> // WARNING: only use version 2.1.9 of this library as it's the only one that worked
 #include <Adafruit_PCD8544.h>
 
-byte i = 0;
+byte i = 0; // timer counter
 char rawBUFFER[128]; // double the length of decodedBUFFER for safety
-char decodedBUFFER[64];
-String BUFFER;
-byte aes_key[] = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA};
-byte aes_iv[N_BLOCK] = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA};
+char decodedBUFFER[64]; // this is where we store raw data after encrypt it
+String BUFFER; // data for parsing onto the display
+byte aes_key[] = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA}; // 128-bit key, USE YOUR OWN KEY
+byte aes_iv[N_BLOCK] = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA}; // 128-bit IV, USE YOUR OWN KEY FOR SECURITY
 char SthImportant[32];
+
+// data variables for the LCD display
 byte SAT;
-char LAT[11], LON[11];
-char DATETIME[20];
+char LAT[12], LON[12];
+char DATE[11], TIME[9];
 
 AESLib aesLib;
 // Hardware SPI:
@@ -46,16 +56,24 @@ Adafruit_PCD8544 display = Adafruit_PCD8544(D3, D4, -1);
 
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600); // 9600 for the sake of consistency with the TX
   Serial.println();
   base64_decode(SthImportant, "TWFkZSBieSBKSU5PREsgdW5kZXIgTUlUIGxpY2Vuc2U", 43);
-  Serial.println(SthImportant);
-  Serial.println("booting...");
+  Serial.println(SthImportant); // just some reminder :)
+  Serial.print("booting...");
+  
+  // Wiring config for SX1276 - NODEMCU
+  // NSS   - D8
+  // RESET - D4
+  // DIO0  - D1
+  // for more info about configuring LoRa module, visit https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md#setup
   LoRa.setPins(D8, D0, D1); // NSS RESET DIO0
-  Serial.println(LoRa.begin(868E6));
-  LoRa.setSyncWord(0xF4);
-  LoRa.onReceive(onReceive);
-  aesLib.gen_iv(aes_iv);
+  Serial.println(LoRa.begin(868E6)); // 868mhz is a stable freq for Semtech SX1276, print out the init return
+  LoRa.setSyncWord(0xF4); // just like preamble but can be changed for more security
+  LoRa.onReceive(onReceive); // register the data receive interrupt
+  aesLib.gen_iv(aes_iv); //generate AES IV
+
+  // LCD init procedure
   display.begin();
   display.clearDisplay();
   display.setContrast(50);
@@ -64,12 +82,12 @@ void setup() {
   display.setTextColor(BLACK);
   display.setCursor(0,0);
   display.println("JtagRX v0.1");
-  display.println(SthImportant);
+  display.println(SthImportant); // just some reminder :)
   display.display();
-  delay(1000);
+  delay(1000); // just to make it more like human speed - no one can read that fast
   display.clearDisplay();
   display.display();
-  LoRa.receive(); // start listening
+  LoRa.receive(); // start listening on LoRa
   
 }
 void loop() {
@@ -77,44 +95,64 @@ void loop() {
 }
 
 void onReceive(int packetSize) {
-  // received a packet
-  Serial.print("Received packet '");
-  // read packet
-  for (int i = 0; i < packetSize; i++) {
+  Serial.print("Packet: ");
+  for (int i = 0; i < packetSize; i++) { // reading and saving the packet
     rawBUFFER[i] = LoRa.read();
     Serial.print(rawBUFFER[i]);
   }
-  // print RSSI of packet
-  Serial.print("' with RSSI ");
-  Serial.println(LoRa.packetRssi());
+  Serial.println();
+  Serial.print("with RSSI ");
+  Serial.println(LoRa.packetRssi()); // signal strengh (the lower, the worse)
+  // good:        0 - -80
+  // ok:        -80 - -100
+  // bad:      -100 - -130
+  // chase it: -130 - -140
+  // F: lower than -140
 
-  //prase packet
+  // decode packet
   aesLib.decrypt((byte*)rawBUFFER, sizeof(rawBUFFER), (char*)decodedBUFFER, aes_key, sizeof(aes_key), aes_iv);
-  //S000x LAT  xx.xxxxxx LON xxx.xxxxxx Txx/xx/2021 xx:xx:xx
+  
+  // packet structure: S0000 LAT   0.000000 LON   0.000000 T00/00/2000 00:00:00
   Serial.println(decodedBUFFER);
+  // parse packet
   BUFFER = decodedBUFFER;
   BUFFER.remove(0, 1);
   SAT = BUFFER.toInt();
   BUFFER.remove(0, 8);
-  BUFFER.toCharArray(LAT, 11);
+  BUFFER.toCharArray(LAT, 12);
   BUFFER.remove(0, 15);
-  BUFFER.toCharArray(LON, 11);
+  BUFFER.toCharArray(LON, 12);
   BUFFER.remove(0, 13);
-  BUFFER.toCharArray(DATETIME, 20);
+  BUFFER.toCharArray(DATE, 11);
+  BUFFER.remove(0, 11);
+  BUFFER.toCharArray(TIME, 9);
+
+  // print to serial
   Serial.println(SAT);
   Serial.println(LAT);
   Serial.println(LON);
-  Serial.println(DATETIME);
+  Serial.println(DATE);
+  Serial.println(TIME);
+
+  // print to display
   display.clearDisplay();
   display.setCursor(0,0);
+  display.print("SAT   ");
   display.print(SAT);
   display.setCursor(0,8);
+  display.print("LAT");
   display.print(LAT);
   display.setCursor(0,16);
+  display.print("LON");
   display.print(LON);
   display.setCursor(0,24);
-  display.print(DATETIME);
+  display.print("DATE");
+  display.print(DATE);
+  display.setCursor(0,32);
+  display.print("TIME  ");
+  display.print(TIME);
   display.setCursor(0,40);
+  display.print("RSSI ");
   display.print(LoRa.packetRssi());
   display.display();
 }
--- Jtag_TX/Jtag_TX.ino
+++ Jtag_TX/Jtag_TX.ino
@@ -21,10 +21,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 #include <ESP8266WiFi.h>
 void preinit() {
-  // Global WiFi constructors are not called yet (global class instances like WiFi, Serial... are not yet initialized)..
+  // Global WiFi constructors are not called yet
+  // (global class instances like WiFi, Serial... are not yet initialized)..
   // No global object methods or C++ exceptions can be called in here!
   // The below is a static class method, which is similar to a function, so it's ok.
-  ESP8266WiFiClass::preinitWiFiOff();
+  ESP8266WiFiClass::preinitWiFiOff(); // turn off wifi AP as we don't need that
 }
 
 #include <TinyGPS++.h>
@@ -32,46 +33,48 @@ void preinit() {
 #include <SPI.h>
 #include <AESLib.h> // WARNING: only use version 2.1.9 of this library as it's the only one that worked
 
-long i = 0;
-char BUFFER[64];
-char encodedBUFFER[128];
-byte aes_key[] = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA};
-byte aes_iv[N_BLOCK] = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA};
+long i = 0; // timer counter
+char BUFFER[64]; // this is where we store raw data before encrypt it
+char encodedBUFFER[128]; // double the length of BUFFER for safety
+byte aes_key[] = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA}; // 128-bit key, USE YOUR OWN KEY
+byte aes_iv[N_BLOCK] = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA}; // 128-bit IV, USE YOUR OWN KEY FOR SECURITY
 char SthImportant[32];
 
 TinyGPSPlus gps;
 AESLib aesLib;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // open serial port at 9600 baud for gps communication, and debugging (if needed), can use other pin with Software Serial,
+  // wiring for GPS module: GPS TX to NODEMCU RX only for receiving gps data, NODEMCU TX is for debugging only
   Serial.println();
   base64_decode(SthImportant, "TWFkZSBieSBKSU5PREsgdW5kZXIgTUlUIGxpY2Vuc2U", 43);
-  Serial.println(SthImportant);
+  Serial.println(SthImportant); // just some reminder :)
   Serial.print("Booting... ");
   
   // Wiring config for SX1276 - NODEMCU
   // NSS   - D8
   // RESET - D4
   // DIO0  - D1
+  // for more info about configuring LoRa module, visit https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md#setup
   LoRa.setPins(D8, D4, D1);
-  Serial.println(LoRa.begin(868E6));
-  LoRa.setSyncWord(0xF4);
-  aesLib.gen_iv(aes_iv);
+  Serial.println(LoRa.begin(868E6)); // 868mhz is a stable freq for Semtech SX1276, print out the init return
+  LoRa.setSyncWord(0xF4); // just like preamble but can be changed for more security
+  aesLib.gen_iv(aes_iv); //generate AES IV
 }
 
 void loop() {
-  while (Serial.available() != 0) if (gps.encode(Serial.read())) GPSdecode();
-  if (millis() - i > 1000) {
-    aesLib.encrypt((byte*)BUFFER, sizeof(BUFFER), (char*)encodedBUFFER, aes_key, sizeof(aes_key), aes_iv);
+  while (Serial.available() != 0) if (gps.encode(Serial.read())) GPSdecode(); // if there is data comming from the GPS module, decode it
+  if (millis() - i > 1000) { // send data once every second or so
+    aesLib.encrypt((byte*)BUFFER, sizeof(BUFFER), (char*)encodedBUFFER, aes_key, sizeof(aes_key), aes_iv); // encrypt the data with AES standard
     LoRa.beginPacket();
-    LoRa.print(encodedBUFFER);
+    LoRa.print(encodedBUFFER); // encoded data
     LoRa.endPacket();
-    Serial.println(BUFFER);
-    Serial.println(encodedBUFFER);
+    Serial.println(BUFFER); // human readable data
+    Serial.println(encodedBUFFER); // encoded data for comparison
     i = millis();
   }
 }
 
-void GPSdecode() {
+void GPSdecode() { // parse GPS data to more human readable one
   sprintf(BUFFER, "S%04d LAT%11f LON%11f T%02d/%02d/%02d %02d:%02d:%02d", gps.satellites.value(), gps.location.lat(), gps.location.lng(), gps.date.day(), gps.date.month(), gps.date.year(), gps.time.hour(), gps.time.minute(), gps.time.second());
 }
